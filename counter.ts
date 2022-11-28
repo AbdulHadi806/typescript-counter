2088a9cddcf6671af8b6e9f2c06c6ca9bceb77b2
Abdul Hadi <abdul.806@yahoo.com>
2022-11-29 01:58:46 +0500

    updates

--- counter.ts
+++ counter.ts
@@ -1,14 +1,12 @@
 const input_1 = document.getElementById("number1")! as HTMLInputElement;
 const input_2 = document.getElementById("number2")! as HTMLInputElement;
-const button = document.getElementById("AddButton");
-const inputData = document.getElementById("input-data")
+const button = document.getElementById("AddButton")!;
+const inputData = document.getElementById("input-data")!
 let integer = 0;
 const addNumber = (num1: number, num2: number) => {
-        integer = num1 + num1
+       return integer = num1 + num2
 };
-
-button.addEventListener("click" function(e) {
+button.addEventListener("click", function(e) {
     e.preventDefault()
-    addNumber(+input_1.value, +input_2.value)
-    inputData.innerHTML = integer;
+     inputData.innerHTML = String(addNumber(+input_1.value, +input_2.value))
 });
